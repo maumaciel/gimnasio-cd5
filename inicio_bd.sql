f7986175df3dc6fa8986e7c215bfc58c656882bd
Juan francisco Pell <juanfranciscopell@gmail.com>
2023-05-13 18:38:19 -0300

    Inicio de la estructura de la base de datos.

---
+++ inicio_bd.sql
@@ -0,0 +1,31 @@
+/* 
+Nombre  =  gimnasio_db
+
+Tablas
+	• Roles
+		○ Administrador
+		○ Empleado ( Profesor )
+		○ Recepcionista
+		○ Cliente
+	• Usuarios
+		○ RolId
+	• Entrenamientos
+	• EntrenamientoPorDia
+		○ EntrenamientoId
+		○ Dia
+		○ Horario Entrada
+		○ Horario Salida
+	• Tarifas
+		○ Valor
+		○ CantidadDias
+		○ FechaLimite
+	• Pagos
+		○ TarifaId
+		○ ClienteId
+		○ Fecha
+		○ TipoPagoId
+		○ EstaPagado
+		○ CreadoFecha
+		ModificadoFecha
+	• Auditorias
+    */
